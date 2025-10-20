45ae5218003ec88d4b55062413f10910eda0994a
dlrj0 <wjgus3@naver.com>
2025-10-20 18:39:42 +0900

    네비바 정리

---
+++ app/src/main/java/com/example/tot/ViewPager/MyPageFragment.java
@@ -0,0 +1,11 @@
+package com.example.tot.ViewPager;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tot.R;
+
+public class MyPageFragment extends Fragment {
+    public MyPageFragment(){
+        super(R.layout.fragment_mypage);
+    }
+}
