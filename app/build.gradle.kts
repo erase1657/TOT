c78c93f3d7326e7d6ecf70b6b18d60ae1cab5734
dlrj0 <wjgus3@naver.com>
2025-10-24 18:12:56 +0900

    홈화면 필터버튼

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("nl.bryanderidder:themed-toggle-button-group:1.4.1")
 }
