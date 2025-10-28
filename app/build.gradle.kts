54a4e9d84b3ddae80e10b8dab9f44dbeae9e51e7
erase1657 <erase1657@naver.com>
2025-10-28 17:34:57 +0900

    스케줄 세팅,바텀시트 다이얼로그 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,12 @@ android {
 
 dependencies {
     implementation("com.naver.maps:map-sdk:3.23.0") // 네이버 지도 SDK
-    implementation("nl.bryanderidder:themed-toggle-button-group:1.4.1")
-    implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("nl.bryanderidder:themed-toggle-button-group:1.4.1") //토글 버튼 리스트
+    implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0") //하단 네브바
+    implementation("androidx.core:core-splashscreen:1.0.1") // 스플래시 이미지
+    implementation("com.github.angads25:toggle:1.1.0") // 스위치버튼
+
+    //파이어베이스 관련
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("androidx.credentials:credentials:1.3.0")
@@ -46,6 +49,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/tot/Schedule/ScheduleItem.java
@@ -0,0 +1,70 @@
+package com.example.tot.Schedule;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.GeoPoint;
+public class  ScheduleItem {
+    private String title;
+    private Timestamp startTime; //스케줄 시간(시작), 몇시부터 할지
+    private Timestamp endTime; //스케줄 시간(끝), 몇시까지 할지
+    private GeoPoint place;
+    private String placeName;
+    private Boolean alarm;
+    public ScheduleItem() {}
+
+    public ScheduleItem(String title, Timestamp startTime, Timestamp endTime, GeoPoint place, String placeName, Boolean alarm) {
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.place = place;
+        this.placeName = placeName;
+        this.alarm = alarm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    public GeoPoint getPlace() {
+        return place;
+    }
+
+    public void setPlace(GeoPoint place) {
+        this.place = place;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public Boolean getAlarm() {
+        return alarm;
+    }
+
+    public void setAlarm(String alarm) {
+        this.alarm = Boolean.valueOf(alarm);
+    }
+}
